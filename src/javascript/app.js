0e906680bfc4736e09f96e7ae51e1aee087625b0
kcorkan <kcorkan@rallydev.com>
2014-10-06 10:27:53 -0600

    saves file, works for stories or features

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -6,9 +6,10 @@ Ext.define('CustomApp', {
         {xtype:'container',itemId:'artifact_box', layout: 'hbox', padding: 25},
         {xtype:'container',itemId:'add_scenario_box', layout: 'vbox', flex: 1, padding: 25},
         {xtype:'container',itemId:'scenario_box', flex: 1},
+        {xtype:'container', itemId: 'file-contents-box'},
         {xtype:'tsinfolink'}
     ],
-
+    top_level_type: 'portfolioitem/feature',
     /*
      * collection store for the scenarios on the test set object
      */
@@ -41,6 +42,7 @@ Ext.define('CustomApp', {
          var model = rec.get('_type');
          this._getArtifact(model,rec.get('ObjectID'));
          this._refreshScenarios(rec);
+         this.down('#preview-file').setDisabled(false);
      },
      _getArtifact:function(model_type,obj_id){
          this.logger.log('_getArtifact');
@@ -50,7 +52,7 @@ Ext.define('CustomApp', {
              success: function(model) {
                  model.load(obj_id,{
                      scope: this,
-                     fetch: ['FormattedID','Name','Description'],
+                     fetch: ['FormattedID','Name','Description','PortfolioItem','Project'],
                      callback: this._updateArtifact
                  });
              }
@@ -128,7 +130,7 @@ Ext.define('CustomApp', {
                     padding: 25,
                         items: [{
                             xtype: 'rallytextfield',
-                            fieldLabel: 'Name',
+                            fieldLabel: 'Scenario',
                             labelAlign: 'right',
                             disabled: true,
                             width: 800,
@@ -281,10 +283,19 @@ Ext.define('CustomApp', {
      _buildArtifactBox: function(){
          this.down('#artifact_box').add({
              xtype: 'rallybutton',
-             text: 'Choose Story...',
+             text: 'Choose Artifact...',
              scope: this,
              margin: 10,
              handler: this._chooseArtifact
+         });
+         this.down('#artifact_box').add({
+             xtype: 'rallybutton',
+             text: 'Save',
+             itemId: 'preview-file',
+             scope: this,
+             margin: 10,
+             handler: this._previewFile,
+             disabled: true
          });
          this.down('#artifact_box').add({
              xtype: 'container',
@@ -292,6 +303,8 @@ Ext.define('CustomApp', {
              cls: 'title',
              tpl:'<h3>{FormattedID}:  {Name}</h3><p>{Description}</p>'
          });
+
+
      },
 
      _buildAddScenarioBox: function(){
@@ -299,7 +312,7 @@ Ext.define('CustomApp', {
          this.down('#add_scenario_box').add({
              xtype: 'rallytextfield',
              itemId: 'scenario_name',
-             fieldLabel: 'Name',
+             fieldLabel: 'Scenario',
              width: 800,
              labelAlign: 'right'
          });
@@ -334,6 +347,58 @@ Ext.define('CustomApp', {
       * Generic Utilities:  _fetchRecord, _createRecord
       * 
       */
+    _previewFile: function(){
+        this.logger.log('_previewFile');
+        
+        //Get the feature if the scenario artifact is not the feature.
+        var top_level_artifact = this._getTopLevelArtifact(this.scenario_artifact).then({
+            scope: this,
+            success: this._buildAndSaveFeatureFile,
+            failure: function(error){
+                alert(Ext.String.format('Error fetching the top level artifact: {0}', error));
+            }
+        });
+
+       
+    },
+    _getTopLevelArtifact: function(current_artifact){
+        this.logger.log('_getTopLevelArtifact');
+        var deferred = Ext.create('Deft.Deferred');
+
+        if (current_artifact.get('_type') == this.top_level_type){
+            deferred.resolve(current_artifact);
+        } else {
+            var pi = current_artifact.get('PortfolioItem');
+            if (pi && pi['FormattedID']) {
+                var filter = [{property: 'FormattedID', value: pi['FormattedID']}]
+                this._fetchRecord(this.top_level_type, filter).then({
+                    scope:this,
+                    success: function(record){
+                        deferred.resolve(record);
+                    },
+                    failure: function(error){
+                        deferred.reject(Ext.String.format('Failed to get top level artifact: {0}',error));
+                    }
+                });
+            } else {
+                deferred.reject(Ext.String.format('Story {0} has no feature parent.', current_artifact.get('FormattedID')));
+            }
+        }
+        return deferred.promise;
+    },
+    _buildAndSaveFeatureFile: function(top_level_artifact){
+        var filename = Ext.String.format('{0}.feature', top_level_artifact.get('FormattedID'));
+        this._buildFeatureFile(top_level_artifact).then({
+            scope: this, 
+            success: function(file_contents){
+                Rally.technicalservices.FileUtilities.saveTextAsFile(file_contents,filename);
+            },
+            failure: function(error){
+               alert(Ext.String.format('Error while creating feature file:  {0}', error));
+            }
+        });
+    },
+    
      _fetchRecord: function(model_type, filters){
          this.logger.log('_fetchRecord', model_type, filters);
          var deferred = Ext.create('Deft.Deferred');
@@ -345,6 +410,7 @@ Ext.define('CustomApp', {
                  model.find({
                      filters: filters, 
                      callback: function(record, success){
+                         console.log(success, filters);
                          if (success) {
                              deferred.resolve(record);
                          } else {
@@ -385,22 +451,119 @@ Ext.define('CustomApp', {
          });
          return deferred.promise;
      },
-     /*
-      * 
-      * Gets and Returns the test set record associated with this scenario
-      * 
-      */     
-          _setScenarioContainer: function(record){
-              if (record == undefined) {
-                  this.logger.log(Ext.String.format('TestSet is empty.'));
-              }
-              this.scenarios_test_set = record;
+          _getTestCaseType: function(){
+              return 'Usability';
           },
 
-          _getScenarioContainer: function(){
-              return this.scenarios_test_set;
+          _buildFeatureFile: function(artifact){
+              this.logger.log('_getArtifactFileTreeStore');
+              var deferred = Ext.create('Deft.Deferred');
+              
+              var sc_ids = [];
+              this._getContainerRecords(artifact.get('_ref')).then({
+                  scope: this,
+                  success: function(container_records){
+                      this.logger.log('_getContainerRecords success', container_records);
+                      this._getScenarioRecords(artifact, container_records).then({
+                          scope: this,
+                          success: function(scenario_records){
+                              var file_contents = this._buildFeatureFileContents(artifact, container_records, scenario_records);
+                              deferred.resolve(file_contents);
+                            
+                          },
+                          failure: function(error){
+                              this.logger.log('_getContainerRecords failure',error);
+                              deferred.reject(error);
+                          }
+                      });
+                  }
+              });
+              return deferred.promise;
           },
-          _getTestCaseType: function(){
-              return 'Usability';  //'Scenario';
+          _getContainerRecords: function(artifact_ref){
+              
+              this.logger.log('_getContainerRecords');
+              var deferred = Ext.create('Deft.Deferred');
+              
+              var store = Ext.create('Rally.data.wsapi.Store', {
+                  model: 'userstory',
+                  filters: [{property: 'PortfolioItem', value: artifact_ref}],
+                  autoLoad: true,
+                  listeners: {
+                      load: function(store, data, success) {
+                          if (success) {
+                              deferred.resolve(store.getRecords());
+                          } else {
+                              deferred.reject('Error querying user stories for portfolio item.')
+                          }
+                      }
+                  },
+                  fetch: ['Name', 'FormattedID', 'Description']
+              });
+              return deferred.promise; 
           },
+          _getScenarioRecords: function(artifact, container_records){
+              this.logger.log('_getScenarioRecords');
+              var deferred = Ext.create('Deft.Deferred');
+              
+              var filter = Ext.create('Rally.data.wsapi.Filter', {
+                  property: 'TestFolder.Name',
+                  operator: '=',
+                  value: artifact.get('FormattedID')
+             });
+              Ext.each(container_records, function(cr){
+                  var f  = Ext.create('Rally.data.wsapi.Filter', {
+                      property: 'TestFolder.Name',
+                      operator: '=',
+                      value: cr.get('FormattedID')
+                 });
+                 filter = filter.or(f);
+              },this);
+              
+              if (filter){
+                  this.logger.log('_getScenarioRecords filter=', filter.toString());
+              }
+              var store = Ext.create('Rally.data.wsapi.Store', {
+                  model: 'testcase',
+                  filters: [filter],
+                  autoLoad: true,
+                  listeners: {
+                      load: function(store, data, success) {
+                          if (success) {
+                              deferred.resolve(store.getRecords());
+                          } else {
+                              deferred.reject('Error creating TestCase store.')
+                          }
+                      }
+                  },
+                  fetch: ['Name', 'FormattedID', 'Description', 'TestFolder','Name']
+              });
+              return deferred.promise; 
+          },
+          _buildFeatureFileContents: function(artifact, containers, scenarios){
+              this.logger.log('_getFeatureFileContents', artifact, containers, scenarios);
+              
+              var file_contents = Ext.String.format('Feature: {0}\n\n', artifact.get('Name'));
+              file_contents += this._buildScenarioFileContentSnippet(artifact.get('FormattedID'),scenarios);
+              
+              Ext.each(containers, function(child){
+                file_contents += Ext.String.format('{0}\n',child.get('Name'));
+                if (child.get('Description')){
+                    file_contents += Ext.String.format('{0}\n',child.get('Description'));
+                }
+                file_contents += '\n';
+                file_contents += this._buildScenarioFileContentSnippet(child.get('FormattedID'),scenarios);
+              }, this);
+              return file_contents; 
+          },
+          _buildScenarioFileContentSnippet: function(formatted_id, scenarios){
+              var snippet = '';
+              Ext.each(scenarios, function(scenario){
+                  var scenario_parent = scenario.get('TestFolder').Name;
+                  if (scenario_parent == formatted_id){
+                      snippet = snippet + Ext.String.format('Scenario: {0}\n{1}\n\n',scenario.get('Name'),scenario.get('Description'));
+                 }
+              }, this);
+              return snippet;  
+          }
 });
